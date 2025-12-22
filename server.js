5d203563dfe95254fcd0d27470c0fb2049cac285
YATHISHKUMAR21 <appuaishu5@gmail.com>
2025-12-22 22:32:12 +0530

    socket io added and initialised

--- server.js
+++ server.js
@@ -1,5 +1,23 @@
-const app = require("./src/app")
+const app = require("./src/app");
+const http = require("http");
+const { Server } = require("socket.io");
 
-app.listen(3000), ()=>{
-    console.log("server is running on port 3000")
-}
+const httpServer = http.createServer(app);
+
+const io = new Server(httpServer, {
+  cors: {
+    origin: "*",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("New client connected:", socket.id);
+
+  socket.on("disconnect", () => {
+    console.log("Client disconnected:", socket.id);
+  });
+});
+
+httpServer.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
